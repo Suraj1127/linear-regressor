f93f51d9a24177966f19a456715abed2cd524160
BarnaSir <sudarshanregmi9@gmail.com>
2018-06-01 11:57:06 +0545

    get input from csv and get output from csv

--- linear_regression.py
+++ linear_regression.py
@@ -1,41 +1,102 @@
+#!/usr/bin/env python3
+
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 class LinearRegression:
+    """
+    Author: Suraj Regmi
+    Date: 31st May, 2018
+    Description: Simple LinearRegression class that builts the linear
+    regression model and then predicts the output from input values.
+    Also, validates the model using root mean square error and coefficient
+    of determination.
+    """
 
     def __init__(self, x, y):
+        """
+        Initialize the model with training data
+        :param x: Numpy array of training input
+        :param y: Numpy array of labelled output
+
+        Size of matrix x: number of training examples * number of features of training examples
+        Size of matrix y: number of training examples * 1
+        """
+
+        # Splitting the x and y matrix into train and test set
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.2, random_state=20)
         self.m, self.n = x.shape
-        self.w = np.random.randn(self.n,1)
-        self.c = np.random.randn(1,1)
+
+        # Initialize the weights and biases
+        self.w = np.random.randn(self.n, 1)
+        self.c = np.random.randn(1, 1)
 
     def train(self, lambd, alpha):
-        self.w = self.w - alpha*(np.matmul(self.x_train.T, np.matmul(self.x_train, self.w)+self.c-self.y_train)/self.m+lambd*self.w/self.m)
+        """
+        One iteration of training
+        :param lambd: regularization parameter
+        :param alpha: learning rate
+        """
+        self.w = self.w - alpha*(np.matmul(self.x_train.T, np.matmul(self.x_train, self.w)
+                                           + self.c-self.y_train)/self.m+lambd*self.w/self.m)
         self.c = self.c - alpha*np.sum(np.matmul(self.x_train, self.w)+self.c-self.y_train)/self.m
 
     def fit(self, alpha, no_of_iterations, lambd):
-        for i in range(no_of_iterations):
+        """
+        Fits the training data to the linear model.
+        :param alpha: learning rate
+        :param no_of_iterations: no of iterations of gradient descent algorithm
+        :param lambd: regularization parameter
+        """
+
+        # Training process to the given number of iterations
+        for _ in range(no_of_iterations):
             self.train(lambd, alpha)
 
     def predict(self, x):
-        print(self.c.shape)
+        """
+        Predicts the value of y on the basis of given value of x.
+        :param x: input value of x, independent variable
+        :return: value of predicted or dependent variable
+        """
         return np.matmul(x, self.w) + self.c
+
+    def validate(self):
+        """
+        Evaluates the performance the model by calculating and printing coefficient
+        of determination and root mean squared error on the test set
+        """
+
+        y_test_pred = self.predict(self.x_test)
+        y_test_mean = np.average(y_test_pred)
+
+        rmse = np.sqrt(np.average((self.y_test-y_test_pred)**2))
+        coef_of_determination = 1-np.sum((self.y_test-y_test_pred)**2)/np.sum((self.y_test-y_test_mean)**2)
+
+        print("Coefficient of determination: {}".format(coef_of_determination))
+        print("Root mean squared error: {}".format(rmse))
 
 
 def main():
 
-    x = np.linspace(0, 10, 100).reshape(-1, 1)
-    print(x.shape)
-    y = 1*x + np.random.randn(x.shape[0], x.shape[1])
+    # Costruct training matrices
+    x = np.array([np.linspace(0, 10, 100), 2*np.linspace(0, 10, 100)]).T
+    y = np.sum(x, axis=1).reshape(-1, 1)
 
+    # Train the model and predict
     linear_regression = LinearRegression(x, y)
-    linear_regression.fit(0.1, 100, 0)
+    linear_regression.fit(0.001, 10000, 0)
     y_pred = linear_regression.predict(x)
-    plt.scatter(x, y)
-    plt.plot(x, y_pred)
+
+    # Print weights, biases and the plot and also print the performance estimators of the model
+    print("Weights: {}\nBiases: {}".format(linear_regression.w, linear_regression.c))
+    plt.plot(y, y_pred)
     plt.show()
+    linear_regression.validate()
 
 
 if __name__ == "__main__":
---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from linear_regression import LinearRegression
+
+
+def get_train_matrices():
+
+    # Read the csv files
+    df_x = pd.read_csv('train/input.csv')
+    df_y = pd.read_csv('train/output.csv')
+
+    # Make training data as numpy arrays
+    x = df_x.values[:, 1:]
+    y = df_y.values[:, 1:]
+
+    return x, y
+
+
+def main():
+
+    # Get training matrices for linear regression model
+    x, y = get_train_matrices()
+
+    # Create instance of LinearRegression with the training matrices
+    linear_regression = LinearRegression(x, y)
+
+    # Fit with learning rate, no of iterations and regularization(L2) parameter
+    linear_regression.fit(0.01, 1000, 0)
+
+    # Predict for all the input values
+    y_pred = linear_regression.predict(x)
+
+    # Plot the scatter plots of training data and graph of our linear model
+    plt.scatter(x, y)
+    plt.plot(x, y_pred)
+    plt.show()
+
+    # Print the weights and biases of the model
+    print("Weights: {}\nBiases: {}".format(linear_regression.w, linear_regression.c))
+
+    # Validate the model by printing the performance metrics
+    linear_regression.validate()
+
+    # Predict for the input data in test folder and save as output.csv in test folder
+    x_test = pd.read_csv('test/input.csv')['x'].values.reshape(-1,1)
+    y_test = linear_regression.predict(x_test)
+    df_predict = pd.DataFrame({'y': y_test.reshape(-1)})
+    df_predict.to_csv('test/output.csv')
+
+
+if __name__ == "__main__":
+    main()
